dc54843b936b334c1bd1343d0babd05326bb9089
Matias2388 <106512472+Matias2388@users.noreply.github.com>
2022-05-31 08:45:51 +0200

    docs: actualizo archivo readme

---
+++ romano.py
@@ -0,0 +1,26 @@
+def converti_en_romano (numero):
+    """
+Restricciones:
+- es un numero natural
+- el numero esta entre 0 y 3999
+- no es mayor que 3999
+
+Resultado es una cadena que contiene (I,V,X,L,C,D,M)
+
+"""
+return "test"
+
+print (convertir_en_romano(33))
+
+"""
+1.validar una entrada: 
+1.1 Si es valido lo vonvierto
+1.2 S no es valido; es un error
+
+
+"""
+# proxima clase!
+# descomponer numero en unidades, decenas, centenas y unidades de millar
+# opcion 1 = dvision entera + modulo cascada
+# opcion 2 = convertir en cadena y en funcion de la longitud y la posicion obtener u,c,d y um
+# en una tupla quizas
